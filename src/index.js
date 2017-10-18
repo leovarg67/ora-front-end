42974cff5653c4eb75a5ae7e012a66e9f8bb2a46
leo <leovarg67@gmail.com>
2017-10-18 18:23:43 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './routes';
+import { SocketProvider } from 'socket.io-react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './redux/store'
+import registerServiceWorker from './registerServiceWorker';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:8080');
+
+ReactDOM.render(
+  <SocketProvider socket={socket}>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </Provider>
+  </SocketProvider >,document.getElementById('root')
+);
+registerServiceWorker();
